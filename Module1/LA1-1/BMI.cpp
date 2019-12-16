4d8c27681d43208f3e424071aa74cf03cc60bbdd
Anthony Garcia <anthonygarcia2@ad.weber.edu>
2019-12-16 11:43:05 -0700

    Learning Assignment: Completed BMI calculator to print out ranges.

--- Module1/LA1-1/BMI.cpp
+++ Module1/LA1-1/BMI.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
+constexpr float KILOGAMS_TO_POUNDS = 2.20462f;
+constexpr float METERS_TO_INCHES = 39.3701f;
+
+enum class EBMIRange
+{
+    Underwieght,
+    Healthy,
+    Overweight,
+    Obese
+};
+
+float CalculateBMIUsingMetric(float weight_kg, float height_m);
+float CalculateBMIUsingEmperical(float weight_lbs, float height_in);
+void PrintBMIRangeMessage(EBMIRange BMIRange);
+void PrintBMIRangeMessage(float BMIvalue);
+EBMIRange GetBMIRange(float BMIvalue);
+
 int main()
 {
-    // Calcualte BMI: weight(kg)/[height(m)]^2
+    // Calcualtes BMI: weight(kg)/[height(m)]^2
     float Weight = 0.0f;
     float Height = 0.0f;
     float BMI = 0.0f;
@@ -16,13 +33,80 @@ int main()
     // Calculate and print out user's bmi
     if (Height > 0.0f)
     {
-        BMI = Weight/(Height*Height);
-        std::cout << "You'r BMI is: " << BMI;
+        BMI = CalculateBMIUsingMetric(Weight, Height);
+        std::cout << "You'r BMI in metric units is: " << BMI << std::endl;
+        BMI = CalculateBMIUsingEmperical(Weight/KILOGAMS_TO_POUNDS, Height/METERS_TO_INCHES);
+        std::cout << "You'r BMI in emperical units is: " << BMI << std::endl;
+
+        // Calculate and print BMI using emperical
+        PrintBMIRangeMessage(BMI);
     }
     else
     {
-        std::cout << "Height cannot be less than or equal to zero";
+        std::cout << "Height cannot be less than or equal to zero\n";
     }
 
     return 0;
+}
+
+float CalculateBMIUsingMetric(float weight_kg, float height_m)
+{
+    float BMI = 0.0f;
+    
+    BMI = weight_kg/(height_m*height_m);
+
+    return BMI;
+}
+
+float CalculateBMIUsingEmperical(float weight_lbs, float height_in)
+{
+    const float BMI_COEFFICIENT = 703.0f;
+
+    return CalculateBMIUsingMetric(weight_lbs/BMI_COEFFICIENT, height_in);
+}
+
+EBMIRange GetBMIRange(float BMIvalue)
+{
+    const float kUnderweightLimit = 18.5f;
+    const float kHealthyLimit = 24.9f;
+    const float kOverweightLimit = 29.9f;
+
+    if(BMIvalue < kUnderweightLimit)
+        return EBMIRange::Underwieght;
+    else if (BMIvalue < kHealthyLimit)
+        return EBMIRange::Healthy;
+    else if (BMIvalue < kOverweightLimit)
+        return EBMIRange::Overweight;
+    else
+        return EBMIRange::Obese;
+}
+
+void PrintBMIRangeMessage(EBMIRange BMIRange)
+{
+    std::cout << "You'r BMI range is: ";
+    switch (BMIRange)
+    {
+        case EBMIRange::Underwieght:
+            std::cout << "Underweight\n";
+            break;
+        case EBMIRange::Healthy:
+            std::cout << "Healthy\n";
+            break;
+        case EBMIRange::Overweight:
+            std::cout << "Overweight\n";
+            break;
+        case EBMIRange::Obese:
+            std::cout << "Obese\n";
+            break;
+        default:
+            std::cout << "ERROR: Could not find range\n";
+    }
+    
+    return;
+}
+
+void PrintBMIRangeMessage(float BMIvalue)
+{
+    EBMIRange BMIRange = GetBMIRange(BMIvalue);
+    PrintBMIRangeMessage(BMIRange);
 }
