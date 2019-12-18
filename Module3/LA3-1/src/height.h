544093bf5d199dac0c561ca64e8dbab91f61f216
Anthony Garcia <anthonygarcia2@ad.weber.edu>
2019-12-18 14:50:47 -0700

    CMake fix to Module 3

---
+++ Module3/LA3-1/src/height.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Height
+{
+private:
+    float m_inches;
+
+public:
+    float Inches() { return m_inches; }
+    void SetInches(float heightInches) { m_inches = heightInches; }
+};
